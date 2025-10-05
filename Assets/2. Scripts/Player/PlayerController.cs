cdbfd92878235357c107bb632c56fb908d5e0660
JBS0731 <heeno7700@gmail.com>
2025-10-05 15:34:37 +0900

    [Fix] : Unity 버전 변경

--- Assets/2. Scripts/Player/PlayerController.cs
+++ Assets/2. Scripts/Player/PlayerController.cs
@@ -1,10 +1,31 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public StateMachine stateMachine;
+    public InputSystem_Actions actions;
+
+    void Awake()
+    {
+        stateMachine = new StateMachine();
+        actions = new InputSystem_Actions();
+        stateMachine.ChangeState(new PlayerIdleState(this));
+    }
+
+    private void OnEnable() => actions.Enable();
+    private void OnDisable() => actions.Disable();
+
+    void Update()
+    {
+        HandleInput();
+        stateMachine.Update();
+    }
     
-    
-    
-    
+
+    void HandleInput()
+    {
+        stateMachine.ChangeState(new PlayerWalkState());
+    }
 }
